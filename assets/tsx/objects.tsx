3cf0348746b3096e4b7bf8e94bae70fa94372add
richardios275 <kaylabridget1112@proton.me>
2025-06-23 03:37:37 +0700

    Feat: Added NEW map

--- assets/tsx/objects.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="16" tileheight="16" tilecount="208" columns="16">
- <image source="../tilesets/mystic_woods_free_2.1/sprites/objects/objects.png" width="256" height="208"/>
-</tileset>
--- assets/tsx/player.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="player" tilewidth="16" tileheight="16" tilecount="540" columns="18">
- <image source="../tilesets/mystic_woods_free_2.1/sprites/characters/player.png" width="288" height="480"/>
-</tileset>
